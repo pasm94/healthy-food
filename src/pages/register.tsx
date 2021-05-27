69dcc591dfd31f0092836c7f662152d6c8c25e3f
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-05-27 02:43:13 -0300

    Register finished

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,136 @@
-import React from 'react';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import { Container, Form } from '@/styles/pages/Register';
+import { FaHome } from 'react-icons/fa';
+import React, { FormEvent, useState } from 'react';
+import { parseCookies, setCookie } from 'nookies';
+import Footer from '@/components/Footer';
 
-const Register = () => {
-  return <h1>register</h1>;
-};
+interface Address {
+  cep: number;
+  uf: string;
+  localidade: string; // city
+}
 
-export default Register;
+export default function Register() {
+  const [name, setName] = useState('');
+  const [birthday, setBirthday] = useState(new Date());
+  const [cpf, setCpf] = useState('');
+  const [cepp, setCepp] = useState<number | null>(null);
+
+  const [invalidData, setInvalidData] = useState(false);
+  const [userAlreadyExists, setUserAlreadyExists] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    setLoading(false);
+    setLoading(true);
+    setSuccess(false);
+
+    const { 'healthyfood.user': cookieUser } = parseCookies();
+
+    const registeredUser = JSON.parse(cookieUser);
+    if (registeredUser.cpf == cpf) {
+      setUserAlreadyExists(true);
+      setInvalidData(false);
+      setLoading(false);
+      return;
+    }
+
+    let address: Address | null = null;
+    try {
+      address = await fetch(`https://viacep.com.br/ws/${cepp}/json/`)
+        .then(response => response.json())
+        .then(data => data);
+    } catch {
+      setInvalidData(true);
+      setUserAlreadyExists(false);
+      setLoading(false);
+      return;
+    }
+
+    setInvalidData(false);
+
+    if (!name || !birthday || !cpf) {
+      setInvalidData(true);
+      setLoading(false);
+      return;
+    }
+
+    const { cep, localidade, uf } = address;
+
+    const user = JSON.stringify({
+      name,
+      birthday,
+      cpf,
+      cep,
+      localidade,
+      uf,
+    });
+
+    setCookie(undefined, 'healthyfood.user', user, {
+      path: '/',
+    });
+
+    setName('');
+    setBirthday(new Date());
+    setCpf('');
+    setCepp(null);
+    setSuccess(true);
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <Container>
+        <span>
+          <FaHome />
+          <a href='/'>Home Page</a>
+        </span>
+        <h1>Sign Up</h1>
+        <Form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <Input
+            // required
+            placeholder='Name'
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+
+          <label>Birthday</label>
+          <Input
+            // required
+            type='date'
+            value={birthday}
+            onChange={event => setBirthday(event.target.value)}
+          />
+
+          <label>CPF</label>
+          <Input
+            // required
+            placeholder='CPF'
+            value={cpf}
+            onChange={event => setCpf(event.target.value)}
+          />
+
+          <label>CEP</label>
+          <Input
+            // required
+            type='number'
+            placeholder='CEP'
+            value={cepp}
+            onChange={event => setCepp(event.target.value)}
+          />
+
+          <Button type='submit'>{loading ? 'One moment...' : 'Sign Up'}</Button>
+        </Form>
+        {invalidData && <h2>You must fill in the fields with valid data</h2>}
+        {userAlreadyExists && <h2>You are already registered</h2>}
+        {success && <h3>Successfully registered</h3>}
+      </Container>
+      <Footer />
+    </>
+  );
+}
---
+++ src/styles/pages/Register.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-image: url('/components/LandingPage/JoinOurMembership/bloco_final_image.svg');
+  background-repeat: no-repeat;
+  background-position: right 0;
+
+  @media (max-width: 890px) {
+    background-size: 40rem;
+  }
+
+  @media (max-width: 790px) {
+    background-size: 30rem;
+  }
+
+  @media (max-width: 594px) {
+    background-size: 20rem;
+  }
+
+  max-width: 100vw;
+  height: 85vh;
+  padding-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+
+  /* align-items: center; */
+  margin-left: 10rem;
+
+  @media (max-width: 594px) {
+    margin-left: 5rem;
+  }
+  max-width: 1366px;
+
+  h1 {
+    margin-bottom: 1rem;
+    margin-top: 3rem;
+    color: #1d164d;
+    font-size: 2rem;
+  }
+
+  h2 {
+    margin-top: 2rem;
+    color: red;
+    font-family: 'Mulish', sans-serif;
+    font-size: 1.2rem;
+  }
+
+  h3 {
+    margin-top: 2rem;
+    color: green;
+    font-family: 'Mulish', sans-serif;
+    font-size: 1.2rem;
+  }
+
+  span {
+    /* margin-left: auto; */
+    /* margin-right: 15rem; */
+
+    font-size: 1.3rem;
+    display: flex;
+    align-items: center;
+
+    a {
+      margin-left: 1rem;
+      text-decoration: line-through;
+      color: #1d164d;
+      font-family: 'Mulish', sans-serif;
+      font-weight: bold;
+      text-decoration: none;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  label {
+    color: #1d164d;
+    font-size: 1.1rem;
+    font-family: 'Mulish', sans-serif;
+  }
+
+  input {
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  button {
+    width: 10rem;
+  }
+`;
